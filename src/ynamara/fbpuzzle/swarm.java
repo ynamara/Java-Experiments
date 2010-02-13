1f417efd13d850f96ef0c7b934141a23ba09229f
Yogy Namara <yogy.namara@gmail.com>
2010-02-13 01:18:17 -0800

    + swarm

--- src/ynamara/fbpuzzle/swarm.java
+++ src/ynamara/fbpuzzle/swarm.java
@@ -6,20 +6,28 @@ import java.util.*;
 public class swarm {
 	static final int MAX_T = 1000;
 	static final int MAX_Z = 1000;
-	
-	void solve(Scanner in) {
-		int[] W = new int[MAX_T];
-		int[] maxV = new int[MAX_Z + 1];
 
-		ArrayList<Integer>[] pred = (ArrayList<Integer>[]) new ArrayList[MAX_Z + 1];
-		for (int i = 0; i < pred.length; i++) {
-			pred[i] = new ArrayList<Integer>();
+	int[] W = new int[MAX_T];
+	int[] maxV = new int[MAX_Z + 1];
+
+	int[][] pred = new int[MAX_T][MAX_Z + 1];
+	static final int NONE = -1;
+	
+	String trace(int r, int c) {
+		if (r < 0) {
+			return "";
+		} else {
+			int t = pred[r][c];
+			return (t == NONE) ? trace(r - 1, c) : trace(r - 1, c - W[t]) + (" " + t + " " + W[t]);
 		}
-		
+	}
+	
+	void solve(Scanner in) {		
 		for (int P = in.nextInt(); P --> 0;) {
 			int T = in.nextInt();
 			int Z = in.nextInt();
 			for (int t = 0; t < T; t++) {
+				Arrays.fill(pred[t], NONE);
 				int s = in.nextInt();
 				int m = in.nextInt();
 				W[t] = s * 3;
@@ -27,26 +35,18 @@ public class swarm {
 					int v2 = maxV[w - W[t]] + m;
 					if (v2 > maxV[w]) {
 						maxV[w] = v2;
-						pred[w].clear();
-						pred[w].addAll(pred[w - W[t]]);
-						pred[w].add(t);
+						pred[t][w] = t;
 					}
 				}
 			}
+			
 			while (Z > 0 && maxV[Z] == maxV[Z - 1]) {
 				Z--;
 			}
 			System.out.println(Z + " " + maxV[Z]);
-			String out = "";
-			for (int t: pred[Z]) {
-				out += t + " " + W[t] + " ";
-			}
-			System.out.println(out.trim());
-			
-			Arrays.fill(maxV, 0);			
-			for (ArrayList<Integer> list: pred) {
-				list.clear();
-			}
+			System.out.println(trace(T - 1, Z).trim());
+
+			Arrays.fill(maxV, 0);
 		}
 	}
 	
