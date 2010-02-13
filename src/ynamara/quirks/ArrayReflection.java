8f94f658a2bc7c54937d3cad211ba724e8f7b2aa
Yogy Namara <yogy.namara@gmail.com>
2010-02-13 02:41:14 -0800

    + linked to blog

--- src/ynamara/quirks/ArrayReflection.java
+++ src/ynamara/quirks/ArrayReflection.java
@@ -1,3 +1,5 @@
+/* http://ynamara.blogspot.com/2010/02/beware-when-reflecting-on-java-arrays.html */
+
 package ynamara.quirks;
 
 import java.util.Arrays;
@@ -8,14 +10,14 @@ public class ArrayReflection {
       System.out.println(arr.length);
       System.out.println(Arrays.toString(arr.clone()));
 
-      // http://bugs.sun.com/bugdatabase/view_bug.do?bug_id=5047859
+      /* http://bugs.sun.com/bugdatabase/view_bug.do?bug_id=5047859 */
       try {
     	  System.out.println(arr.getClass().getField("length"));
       } catch (Exception e) {
     	  e.printStackTrace();
       }
 
-      // http://bugs.sun.com/bugdatabase/view_bug.do?bug_id=4987375
+      /* http://bugs.sun.com/bugdatabase/view_bug.do?bug_id=4987375 */
       try {
     	  System.out.println(arr.getClass().getMethod("clone"));
       } catch (Exception e) {
