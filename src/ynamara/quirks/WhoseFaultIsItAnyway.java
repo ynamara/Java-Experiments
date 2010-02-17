e5c01f20b88a8bb279bae36ce9be7f699a56a0fe
Yogy Namara <yogy.namara@gmail.com>
2010-02-16 18:41:37 -0800

    rename

---
+++ src/ynamara/quirks/WhoseFaultIsItAnyway.java
@@ -0,0 +1,12 @@
+package ynamara.quirks;
+/* http://bugs.sun.com/bugdatabase/view_bug.do?bug_id=4730877 */
+
+public class WhoseFaultIsItAnyway {
+	public static void main(String[] args) {
+		Error yours = new Error("You");
+		Error mine = new Error("Me");
+		yours.initCause(mine);
+		mine.initCause(yours);
+		mine.printStackTrace();
+	}
+}
