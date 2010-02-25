5e5df14d58741a4c2d524708daf5648b2ef33c77
Yogy Namara <yogy.namara@gmail.com>
2010-02-24 23:58:09 -0800

    v0

---
+++ src/ynamara/quirks/MeSoDeep.java
@@ -0,0 +1,41 @@
+package ynamara.quirks;
+
+import java.util.Arrays;
+
+public class MeSoDeep {
+	public static Object[] selfContainingArray() {
+		Object[] arr = new Object[1];
+		arr[0] = arr;
+		return arr;
+	}
+	public static void main(String[] args) {
+		try {
+			System.out.println(
+				Arrays.deepToString(
+					selfContainingArray()
+				)
+			);
+		} catch (StackOverflowError e) {
+			System.out.println(e);
+		} // prints "[[...]]"
+		try {
+			System.out.println(
+				Arrays.deepHashCode(
+					selfContainingArray()
+				)
+			);
+		} catch (StackOverflowError e) {
+			System.out.println(e);
+		} // stack overflows
+		try {
+			System.out.println(
+				Arrays.deepEquals(
+					selfContainingArray(),
+					selfContainingArray()
+				)
+			);
+		} catch (StackOverflowError e) {
+			System.out.println(e);
+		} // stack overflows
+	}
+}
--- src/ynamara/quirks/WhoseFaultIsItAnyway.java
+++ src/ynamara/quirks/WhoseFaultIsItAnyway.java
@@ -1,5 +1,6 @@
-package ynamara.quirks;
 /* http://bugs.sun.com/bugdatabase/view_bug.do?bug_id=4730877 */
+
+package ynamara.quirks;
 
 public class WhoseFaultIsItAnyway {
 	public static void main(String[] args) {
