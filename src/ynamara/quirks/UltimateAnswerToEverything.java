7a0a1ca49a614c9420c14609a989647d5d484e00
Yogy Namara <yogy.namara@gmail.com>
2010-03-19 12:50:14 +0700

    omg

--- src/ynamara/quirks/UltimateAnswerToEverything.java
+++ src/ynamara/quirks/UltimateAnswerToEverything.java
@@ -1,36 +1,24 @@
 // http://www.polygenelubricants.com/2010/03/modifying-static-final-fields-through.html
 
-package ynamara.quirks;
-
 /* 
- * 
  *  "Six by nine. Forty two."
  *  "That's it. That's all there is."
  *  "I always thought something was fundamentally wrong with the universe."
  * 
- *                The Restaurant at the End of the Universe (Douglas Adams)
- *              
+ *                       The Restaurant at the End of the Universe
+ *                                                (Douglas Adams)
  */
 
-import java.lang.reflect.*;
+package ynamara.quirks;
 
 public class UltimateAnswerToEverything {
-	static void setFinalStatic(Field field, Object newValue) throws Exception {
-		field.setAccessible(true);
-		
-		Field modifiersField = Field.class.getDeclaredField("modifiers");
-		modifiersField.setAccessible(true);
-		modifiersField.setInt(field, field.getModifiers() & ~Modifier.FINAL);
-
-		field.set(null, newValue);
-	}
 	static Integer[] ultimateAnswer() {
 		Integer[] ret = new Integer[256];
 		java.util.Arrays.fill(ret, 42);
 		return ret;
 	}	
 	public static void main(String args[]) throws Exception {
-		setFinalStatic(
+		EverythingIsTrue.setFinalStatic(
 			Class.forName("java.lang.Integer$IntegerCache")
 				.getDeclaredField("cache"),
 			ultimateAnswer()
