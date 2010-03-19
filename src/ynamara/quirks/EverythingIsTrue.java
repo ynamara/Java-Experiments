a9ddc7123991c90a7b931291cd7462523342cfb1
Yogy Namara <yogy.namara@gmail.com>
2010-03-18 20:12:16 -0700

    omg

--- src/ynamara/quirks/EverythingIsTrue.java
+++ src/ynamara/quirks/EverythingIsTrue.java
@@ -1,21 +1,22 @@
+// http://www.polygenelubricants.com/2010/03/modifying-static-final-fields-through.html
+
 package ynamara.quirks;
 
 import java.lang.reflect.*;
 
 public class EverythingIsTrue {
-		static void setFinalStatic(Field field, Object newValue) throws Exception {
+	static void setFinalStatic(Field field, Object newValue) throws Exception {
 		field.setAccessible(true);
+		
 		Field modifiersField = Field.class.getDeclaredField("modifiers");
 		modifiersField.setAccessible(true);
-		modifiersField.setInt(field, 
-			field.getModifiers() & ~Modifier.FINAL
-		);
+		modifiersField.setInt(field, field.getModifiers() & ~Modifier.FINAL);
+
 		field.set(null, newValue);
 	}
 	public static void main(String args[]) throws Exception {		
 		setFinalStatic(Boolean.class.getField("FALSE"), true);
 
-		System.out.format("Everything is %s", false);
-		   // prints "Everything is true"
+		System.out.format("Everything is %s", false); // "Everything is true"
 	}
 }
